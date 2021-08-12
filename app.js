339b6d79e6a786f0ea27e4eb659103d6d85a486a
Dylan Floyd <floyd800@gmail.com>
2021-08-12 16:20:33 -0700

    refactor

--- app.js
+++ app.js
@@ -6,43 +6,63 @@ import {
   divide
 } from './utils.js';
 
+//number inputs
 let numberInput1 = document.getElementById('number-input-1');
 let numberInput2 = document.getElementById('number-input-2');
+
+//buttons
 let addButton = document.getElementById('add-button');
 let subtractButton = document.getElementById('subtract-button');
 let divideButton = document.getElementById('divide-button');
 let multiplyButton = document.getElementById('multiply-button');
+
+//display elements
+let countDisplay = document.querySelector('#counter');
 let resultDisplay = document.getElementById('result-display');
 
 // initialize state
+let count = 0;
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 
+//Helper function to get Number value of number-input-1
 let getNumber1 = () => {
   return Number(numberInput1.value);
 }
 
+//Helper function to get Number value of number-input-2
 let getNumber2 = () => {
   return Number(numberInput2.value)
 }
 
+/* Calls the provided math function with the values from
+ * getNumber1() and getNumber2() and updates the display elements.
+ */
+let calculate = (mathFunc) => {
+  resultDisplay.textContent = 'Result: ' + mathFunc(getNumber1(), getNumber2());
+  count++;
+  countDisplay.textContent = 'Calculation Count: ' + count;
+} 
+
+//Set up event listeners:
+
 addButton.addEventListener('click', () => {
-  resultDisplay.textContent = 'Result: ' + add(getNumber1(), getNumber2());
+  calculate(add);
 });
 
 subtractButton.addEventListener('click', () => {
-  resultDisplay.textContent = 'Result: ' + subtract(getNumber1(), getNumber2());
+  calculate(subtract);
 });
 
 multiplyButton.addEventListener('click', () => {
-  resultDisplay.textContent = 'Result: ' + multiply(getNumber1(), getNumber2());
+  calculate(multiply);
 });
 
 divideButton.addEventListener('click', () => {
-  resultDisplay.textContent = 'Result: ' + divide(getNumber1(), getNumber2());
+  calculate(divide);
 });
 
 document.addEventListener('keypress', () => {
