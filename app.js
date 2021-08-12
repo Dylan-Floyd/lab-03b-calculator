c018ac8adbaa886d05f462133426856756183528
Dylan Floyd <floyd800@gmail.com>
2021-08-12 15:22:14 -0700

    add basic calculator feature

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // import functions and grab DOM elements
+import {
+  add,
+  subtract,
+  multiply,
+  divide
+} from './utils.js';
+
+let numberInput1 = document.getElementById('number-input-1');
+let numberInput2 = document.getElementById('number-input-2');
+let addButton = document.getElementById('add-button');
+let subtractButton = document.getElementById('subtract-button');
+let divideButton = document.getElementById('divide-button');
+let multiplyButton = document.getElementById('multiply-button');
+let resultDisplay = document.getElementById('result-display');
 
 // initialize state
 
@@ -6,3 +20,31 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+let getNumber1 = () => {
+  return Number(numberInput1.value);
+}
+
+let getNumber2 = () => {
+  return Number(numberInput2.value)
+}
+
+addButton.addEventListener('click', () => {
+  resultDisplay.textContent = 'Result: ' + add(getNumber1(), getNumber2());
+});
+
+subtractButton.addEventListener('click', () => {
+  resultDisplay.textContent = 'Result: ' + subtract(getNumber1(), getNumber2());
+});
+
+multiplyButton.addEventListener('click', () => {
+  resultDisplay.textContent = 'Result: ' + multiply(getNumber1(), getNumber2());
+});
+
+divideButton.addEventListener('click', () => {
+  resultDisplay.textContent = 'Result: ' + divide(getNumber1(), getNumber2());
+});
+
+document.addEventListener('keypress', () => {
+  resultDisplay.textContent = '';
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,11 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import {
+    add,
+    subtract,
+    multiply,
+    divide
+ } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -15,4 +21,20 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('add 2 and 3 returns 5', (expect) => {
+    expect.equal(5, add(2, 3));
+});
+
+test('2 minus 3 returns -1', (expect) => {
+    expect.equal(-1, subtract(2, 3));
+});
+
+test('10 divided by 2 returns 5', (expect) => {
+    expect.equal(5, divide(10, 2));
+});
+
+test('5 times 3 returns 15', (expect) => {
+    expect.equal(15, multiply(5, 3));
 });
